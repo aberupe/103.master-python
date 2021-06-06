3b50fa92de88afdfa867a9735964e6afd1a87e08
aberupe <anrosalesc@gmail.com>
2021-06-05 20:43:26 -0500

    upd

--- 11-ejercicios/ejercicio1.py
+++ 11-ejercicios/ejercicio1.py
@@ -8,44 +8,51 @@ de 8 numeros enteros y haga lo siguiente:
 - buscar algun elemento (que el usuario pida por teclado)
 """
 
-#crear la lista
-numeros = [13,64,52,73,21,7,91,63]
+# crear la lista
+numeros = [13, 64, 52, 73, 21, 7, 91, 63]
 
-#recorrer y mostrar
+# recorrer y mostrar
 print("####### Recorrer y Mostrar #######")
 for numero in numeros:
     print(numero)
 
-#Crear funcion que recorra y devuelva string
+# Crear funcion que recorra y devuelva string
 print("####### Funcion y devuelve string #######")
+
+
 def mostrarlista(lista):
     resultado = ""
-    for elemento in lista:  
+    for elemento in lista:
         resultado += "Elemento: " + str(elemento)
         resultado += "\n"
     return resultado
 
+
 print(mostrarlista(numeros))
 
-#Ordenar y mostrarla
+# Ordenar y mostrarla
 print("####### Ordenar y mostrar #######")
 numeros.sort()
 print(numeros)
 
-#Longitud y mostrarla
+# Longitud y mostrarla
 print("####### Mostrar longitud #######")
 print(len(numeros))
 
-#Buscar algun elemento
-print("####### Buscar elemento en la lista #######")
-busqueda = int(input("Introduce el Número: "))
-
-comprobar = isinstance(busqueda, int)
-while not comprobar or busqueda <= 0:
+try:
+    # Buscar algun elemento
+    print("####### Buscar elemento en la lista #######")
     busqueda = int(input("Introduce el Número: "))
-else:
-    print(f"Has introducido el {busqueda}")
-print(f"######Buscar en la lista el numero  {busqueda} #######")
-search = numeros.index(busqueda)
 
-print(f"El número buscado existe en la lista, es el indice:  {search}")
+    comprobar = isinstance(busqueda, int)
+    while not comprobar or busqueda <= 0:
+        busqueda = int(input("Introduce el Número: "))
+    else:
+        print(f"Has introducido el {busqueda}")
+    print(f"######Buscar en la lista el numero  {busqueda} #######")
+
+    search = numeros.index(busqueda)
+
+    print(f"El número buscado existe en la lista, es el indice:  {search}")
+except:
+    print("El numero no esta en la lista, lo siento.")
