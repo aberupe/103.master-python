3d1110fbfc825b706fe7cbb23050287873d42ed2
aberupe <anrosalesc@gmail.com>
2021-06-05 21:22:41 -0500

    upd 05

---
+++ 16-poo-clases/main.py
@@ -0,0 +1,58 @@
+# Programacion orientada a objetos
+
+# Definir una clase (molde para crear mas objetosde este tipo
+# (Coche) con caracteristicas similares)
+
+class Coche:
+
+    # atributos o propiedades (variables)
+    # caracteristicas del coche
+
+    color = "Rojo"
+    marca = "Ferrari"
+    modelo = "Aventador"
+    velocidad = 300
+    caballaje = 500
+    plazas = 2
+
+# metodos, son acciones que hace el objeto (coche) (funciones)
+    def setColor(self, color):
+        self.color = color
+
+    def getColor(self):
+        return self.color
+
+    def setModelo(self, modelo):
+        self.modelo = modelo
+
+    def getModelo(self):
+        return self.modelo
+
+    def acelerar(self):
+        self.velocidad += 1
+
+    def frenar(self):
+        self.velocidad -= 1
+
+    def getVelocidad(self):
+        return self.velocidad
+
+# fin definicion clase
+
+
+# crear objetos / instanciar la clase
+coche = Coche()
+
+coche.setColor("Amarillo")
+coche.setModelo("Murcielago")
+
+print(coche.marca, coche.getModelo(), coche.getColor())
+print("Velocidad actual: ", coche.getVelocidad())
+
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.acelerar()
+coche.frenar()
+
+print("Velocidad nueva: ", coche.getVelocidad())
